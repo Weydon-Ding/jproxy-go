edc342d295a521ab6f392f1d5eb245c324680d23
dingweidong_960828 <Weydon-Ding@outlook.com>
2026-08-05 03:29:35 +0800

    feat(store): expose repository contracts
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ internal/store/sqlite/repository_contract_test.go
@@ -0,0 +1,110 @@
+package sqlite_test
+
+import (
+	"context"
+	"testing"
+
+	"jproxy-go/internal/store/sqlite"
+)
+
+type configFake struct{}
+type userFake struct{}
+type sonarrRuleFake struct{}
+type radarrRuleFake struct{}
+type sonarrTitleFake struct{}
+type radarrTitleFake struct{}
+type tmdbTitleFake struct{}
+
+func (configFake) Get(context.Context, sqlite.SystemConfigID) (sqlite.SystemConfig, error) {
+	return sqlite.SystemConfig{}, nil
+}
+func (configFake) Upsert(context.Context, sqlite.SystemConfig) error        { return nil }
+func (configFake) UpsertBatch(context.Context, []sqlite.SystemConfig) error { return nil }
+func (configFake) List(context.Context) ([]sqlite.SystemConfig, error)      { return nil, nil }
+func (configFake) ValueByKey(context.Context, string) (string, error)       { return "", nil }
+
+func (userFake) Get(context.Context, sqlite.SystemUserID) (sqlite.SystemUser, error) {
+	return sqlite.SystemUser{}, nil
+}
+func (userFake) Upsert(context.Context, sqlite.SystemUser) error { return nil }
+func (userFake) FindByUsername(context.Context, string) (sqlite.SystemUser, error) {
+	return sqlite.SystemUser{}, nil
+}
+
+func (sonarrRuleFake) Get(context.Context, sqlite.RuleID) (sqlite.SonarrRule, error) {
+	return sqlite.SonarrRule{}, nil
+}
+func (sonarrRuleFake) Upsert(context.Context, sqlite.SonarrRule) error { return nil }
+func (sonarrRuleFake) Page(context.Context, sqlite.RuleFilter) (sqlite.PageResult[sqlite.SonarrRule], error) {
+	return sqlite.PageResult[sqlite.SonarrRule]{}, nil
+}
+func (sonarrRuleFake) UpsertBatch(context.Context, sqlite.SonarrRuleBatch) error { return nil }
+func (sonarrRuleFake) DeleteBatch(context.Context, sqlite.RuleIDs) error         { return nil }
+func (sonarrRuleFake) SwitchValidStatus(context.Context, sqlite.RuleIDs, sqlite.ValidStatus) error {
+	return nil
+}
+func (sonarrRuleFake) Replace(context.Context, sqlite.SonarrRuleBatch) error { return nil }
+
+func (radarrRuleFake) Get(context.Context, sqlite.RuleID) (sqlite.RadarrRule, error) {
+	return sqlite.RadarrRule{}, nil
+}
+func (radarrRuleFake) Upsert(context.Context, sqlite.RadarrRule) error { return nil }
+func (radarrRuleFake) Page(context.Context, sqlite.RuleFilter) (sqlite.PageResult[sqlite.RadarrRule], error) {
+	return sqlite.PageResult[sqlite.RadarrRule]{}, nil
+}
+func (radarrRuleFake) UpsertBatch(context.Context, sqlite.RadarrRuleBatch) error { return nil }
+func (radarrRuleFake) DeleteBatch(context.Context, sqlite.RuleIDs) error         { return nil }
+func (radarrRuleFake) SwitchValidStatus(context.Context, sqlite.RuleIDs, sqlite.ValidStatus) error {
+	return nil
+}
+func (radarrRuleFake) Replace(context.Context, sqlite.RadarrRuleBatch) error { return nil }
+
+func (sonarrTitleFake) Get(context.Context, sqlite.SonarrTitleID) (sqlite.SonarrTitle, error) {
+	return sqlite.SonarrTitle{}, nil
+}
+func (sonarrTitleFake) Upsert(context.Context, sqlite.SonarrTitle) error { return nil }
+func (sonarrTitleFake) Page(context.Context, sqlite.SonarrTitleFilter) (sqlite.PageResult[sqlite.SonarrTitle], error) {
+	return sqlite.PageResult[sqlite.SonarrTitle]{}, nil
+}
+func (sonarrTitleFake) UpsertBatch(context.Context, sqlite.SonarrTitleBatch) error   { return nil }
+func (sonarrTitleFake) DeleteBatch(context.Context, sqlite.SonarrTitleIDs) error     { return nil }
+func (sonarrTitleFake) Replace(context.Context, sqlite.SonarrTitleBatch) error       { return nil }
+func (sonarrTitleFake) NeedTMDBSync(context.Context) ([]int64, error)                { return nil, nil }
+func (sonarrTitleFake) WithTMDBTitles(context.Context) ([]sqlite.SonarrTitle, error) { return nil, nil }
+
+func (radarrTitleFake) Get(context.Context, sqlite.RadarrTitleID) (sqlite.RadarrTitle, error) {
+	return sqlite.RadarrTitle{}, nil
+}
+func (radarrTitleFake) Upsert(context.Context, sqlite.RadarrTitle) error { return nil }
+func (radarrTitleFake) Page(context.Context, sqlite.RadarrTitleFilter) (sqlite.PageResult[sqlite.RadarrTitle], error) {
+	return sqlite.PageResult[sqlite.RadarrTitle]{}, nil
+}
+func (radarrTitleFake) UpsertBatch(context.Context, sqlite.RadarrTitleBatch) error { return nil }
+func (radarrTitleFake) DeleteBatch(context.Context, sqlite.RadarrTitleIDs) error   { return nil }
+func (radarrTitleFake) Replace(context.Context, sqlite.RadarrTitleBatch) error     { return nil }
+
+func (tmdbTitleFake) Get(context.Context, sqlite.TMDBTitleID) (sqlite.TMDBTitle, error) {
+	return sqlite.TMDBTitle{}, nil
+}
+func (tmdbTitleFake) Upsert(context.Context, sqlite.TMDBTitle) error { return nil }
+func (tmdbTitleFake) Page(context.Context, sqlite.TMDBTitleFilter) (sqlite.PageResult[sqlite.TMDBTitle], error) {
+	return sqlite.PageResult[sqlite.TMDBTitle]{}, nil
+}
+func (tmdbTitleFake) UpsertBatch(context.Context, sqlite.TMDBTitleBatch) error { return nil }
+func (tmdbTitleFake) DeleteBatch(context.Context, sqlite.TMDBTitleIDs) error   { return nil }
+func (tmdbTitleFake) Replace(context.Context, sqlite.TMDBTitleBatch) error     { return nil }
+func (tmdbTitleFake) FindByTVDBID(context.Context, int64) ([]sqlite.TMDBTitle, error) {
+	return nil, nil
+}
+
+func TestRepositoryContracts_areExportedAndFakeable(_ *testing.T) {
+	var config sqlite.SystemConfigRepository = configFake{}
+	var user sqlite.SystemUserRepository = userFake{}
+	var sonarrRule sqlite.SonarrRuleRepository = sonarrRuleFake{}
+	var radarrRule sqlite.RadarrRuleRepository = radarrRuleFake{}
+	var sonarrTitle sqlite.SonarrTitleRepository = sonarrTitleFake{}
+	var radarrTitle sqlite.RadarrTitleRepository = radarrTitleFake{}
+	var tmdbTitle sqlite.TMDBTitleRepository = tmdbTitleFake{}
+	repositories := sqlite.Repositories{SystemConfigs: config, SystemUsers: user, SonarrRules: sonarrRule, RadarrRules: radarrRule, SonarrTitles: sonarrTitle, RadarrTitles: radarrTitle, TMDBTitles: tmdbTitle}
+	_ = repositories
+}
---
+++ internal/store/sqlite/repository_interfaces.go
@@ -0,0 +1,61 @@
+package sqlite
+
+import "context"
+
+type SystemConfigRepository interface {
+	Get(context.Context, SystemConfigID) (SystemConfig, error)
+	Upsert(context.Context, SystemConfig) error
+	UpsertBatch(context.Context, []SystemConfig) error
+	List(context.Context) ([]SystemConfig, error)
+	ValueByKey(context.Context, string) (string, error)
+}
+type SystemUserRepository interface {
+	Get(context.Context, SystemUserID) (SystemUser, error)
+	Upsert(context.Context, SystemUser) error
+	FindByUsername(context.Context, string) (SystemUser, error)
+}
+type SonarrRuleRepository interface {
+	Get(context.Context, RuleID) (SonarrRule, error)
+	Upsert(context.Context, SonarrRule) error
+	Page(context.Context, RuleFilter) (PageResult[SonarrRule], error)
+	UpsertBatch(context.Context, SonarrRuleBatch) error
+	DeleteBatch(context.Context, RuleIDs) error
+	SwitchValidStatus(context.Context, RuleIDs, ValidStatus) error
+	Replace(context.Context, SonarrRuleBatch) error
+}
+type RadarrRuleRepository interface {
+	Get(context.Context, RuleID) (RadarrRule, error)
+	Upsert(context.Context, RadarrRule) error
+	Page(context.Context, RuleFilter) (PageResult[RadarrRule], error)
+	UpsertBatch(context.Context, RadarrRuleBatch) error
+	DeleteBatch(context.Context, RuleIDs) error
+	SwitchValidStatus(context.Context, RuleIDs, ValidStatus) error
+	Replace(context.Context, RadarrRuleBatch) error
+}
+type SonarrTitleRepository interface {
+	Get(context.Context, SonarrTitleID) (SonarrTitle, error)
+	Upsert(context.Context, SonarrTitle) error
+	Page(context.Context, SonarrTitleFilter) (PageResult[SonarrTitle], error)
+	UpsertBatch(context.Context, SonarrTitleBatch) error
+	DeleteBatch(context.Context, SonarrTitleIDs) error
+	Replace(context.Context, SonarrTitleBatch) error
+	NeedTMDBSync(context.Context) ([]int64, error)
+	WithTMDBTitles(context.Context) ([]SonarrTitle, error)
+}
+type RadarrTitleRepository interface {
+	Get(context.Context, RadarrTitleID) (RadarrTitle, error)
+	Upsert(context.Context, RadarrTitle) error
+	Page(context.Context, RadarrTitleFilter) (PageResult[RadarrTitle], error)
+	UpsertBatch(context.Context, RadarrTitleBatch) error
+	DeleteBatch(context.Context, RadarrTitleIDs) error
+	Replace(context.Context, RadarrTitleBatch) error
+}
+type TMDBTitleRepository interface {
+	Get(context.Context, TMDBTitleID) (TMDBTitle, error)
+	Upsert(context.Context, TMDBTitle) error
+	Page(context.Context, TMDBTitleFilter) (PageResult[TMDBTitle], error)
+	UpsertBatch(context.Context, TMDBTitleBatch) error
+	DeleteBatch(context.Context, TMDBTitleIDs) error
+	Replace(context.Context, TMDBTitleBatch) error
+	FindByTVDBID(context.Context, int64) ([]TMDBTitle, error)
+}
